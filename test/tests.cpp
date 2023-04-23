67e6d45480b1b22aa068f1b91239404eec43f408
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-04-23 10:16:22 -0700

    Insert testing

--- test/tests.cpp
+++ test/tests.cpp
@@ -108,3 +108,50 @@ TEST_CASE("DelChar", "[row]")
     row::DelChar(r, 10);
     CHECK("134567" == r.chars);
 }
+
+TEST_CASE("Insert", "[row]")
+{
+  editorConfig E;
+  E.numrows = 0; // FIXME: should not need this
+  E.dirty = 0;
+
+  // First entry
+  row::Insert(E, 0 , "Hello");
+  CHECK(E.row[0].chars == "Hello");
+  CHECK(E.row[0].size == 5);
+  CHECK(E.row[0].idx == 0);
+  CHECK(E.numrows == 1);
+  CHECK(E.dirty > 0);
+
+  // Second Entry, following first
+  row::Insert(E, 1, "World");
+  CHECK(E.row[1].chars == "World");
+  CHECK(E.row[1].idx == 1);
+  CHECK(E.numrows == 2);
+
+  // Third Entry, before first
+  row::Insert(E, 0, "Top");
+  CHECK(E.row[0].chars == "Top");
+  CHECK(E.row[0].idx == 0);
+  CHECK(E.row[1].chars == "Hello");
+  CHECK(E.row[1].idx == 1);
+  CHECK(E.row[2].chars == "World");
+  CHECK(E.row[2].idx == 2);
+  CHECK(E.numrows == 3);
+
+  // Fourth Entry, after last
+  row::Insert(E, 3, "Bottom");
+  CHECK(E.row[3].chars == "Bottom");
+  CHECK(E.row[3].idx == 3);
+  CHECK(E.row[2].chars == "World");
+  CHECK(E.row[2].idx == 2);
+  CHECK(E.numrows == 4);
+
+  // Fifth Entry, in the middle
+  row::Insert(E, 2, "Middle");
+  CHECK(E.row[2].chars == "Middle");
+  CHECK(E.row[2].idx == 2);
+  CHECK(E.row[4].chars == "Bottom");
+  CHECK(E.row[4].idx == 4);
+  CHECK(E.numrows == 5);
+}

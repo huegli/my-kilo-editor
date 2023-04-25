fcddb6e6331a61ea84dfff33290261f7c0910d1b
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-04-25 11:33:01 -0700

    Testing AppendString

--- test/tests.cpp
+++ test/tests.cpp
@@ -57,13 +57,13 @@ bool checkUpdate(const std::string &chars, const std::string &render)
 }
 
 std::array<std::pair<std::string, std::string>, 8> updateArray{ { { "\t", "        " },
-  { "#\t#", "#       #" }, { "##\t#", "##      #" },
+  { "#\t#", "#       #" },
+  { "##\t#", "##      #" },
   { "###\t#", "###     #" },
   { "####\t#", "####    #" },
   { "#####\t#", "#####   #" },
   { "######\t#", "######  #" },
   { "########\t#", "########        #" } } };
-
 
 
 TEST_CASE("Update", "[row]")
@@ -73,38 +73,52 @@ TEST_CASE("Update", "[row]")
 
 TEST_CASE("InsertChar", "[row]")
 {
-    row::erow r;
-    r.chars = "0123456789";
-    r.size = 10;
-    row::InsertChar(r,0, '*');
-    CHECK("*0123456789" == r.chars);
-    row::InsertChar(r,1, '^');
-    CHECK("*^0123456789" == r.chars);
-    row::InsertChar(r, 11, '$');
-    CHECK("*^012345678$9" == r.chars);
-    row::InsertChar(r, 13, '#');
-    CHECK("*^012345678$9#" == r.chars);
-    row::InsertChar(r, -10, '@');
-    CHECK("*^012345678$9#@" == r.chars);
-    row::InsertChar(r, 9999, '!');
-    CHECK("*^012345678$9#@!" == r.chars);
+  row::erow r;
+  r.chars = "0123456789";
+  r.size = 10;
+  row::InsertChar(r, 0, '*');
+  CHECK("*0123456789" == r.chars);
+  row::InsertChar(r, 1, '^');
+  CHECK("*^0123456789" == r.chars);
+  row::InsertChar(r, 11, '$');
+  CHECK("*^012345678$9" == r.chars);
+  row::InsertChar(r, 13, '#');
+  CHECK("*^012345678$9#" == r.chars);
+  row::InsertChar(r, -10, '@');
+  CHECK("*^012345678$9#@" == r.chars);
+  row::InsertChar(r, 9999, '!');
+  CHECK("*^012345678$9#@!" == r.chars);
+}
+
+TEST_CASE("AppendString", "[row")
+{
+  row::erow r;
+  r.chars = "0123456789";
+  r.size = 10;
+  row::AppendString(r, "ABC");
+  CHECK("0123456789ABC" == r.chars);
+  row::AppendString(r, "DEF");
+  CHECK("0123456789ABCDEF" == r.chars);
+  row::AppendString(r, "GHI");
+  CHECK("0123456789ABCDEFGHI" == r.chars);
+  CHECK(19 == r.size);
 }
 
 TEST_CASE("DelChar", "[row]")
 {
-    row::erow r;
-    r.chars = "0123456789";
-    r.size = 10;
-    row::DelChar(r,0);
-    CHECK("123456789" == r.chars);
-    row::DelChar(r,1);
-    CHECK("13456789" == r.chars);
-    row::DelChar(r, 6);
-    CHECK("1345679" == r.chars);
-    row::DelChar(r, 6);
-    CHECK("134567" == r.chars);
-    row::DelChar(r, -10);
-    CHECK("134567" == r.chars);
-    row::DelChar(r, 10);
-    CHECK("134567" == r.chars);
+  row::erow r;
+  r.chars = "0123456789";
+  r.size = 10;
+  row::DelChar(r, 0);
+  CHECK("123456789" == r.chars);
+  row::DelChar(r, 1);
+  CHECK("13456789" == r.chars);
+  row::DelChar(r, 6);
+  CHECK("1345679" == r.chars);
+  row::DelChar(r, 6);
+  CHECK("134567" == r.chars);
+  row::DelChar(r, -10);
+  CHECK("134567" == r.chars);
+  row::DelChar(r, 10);
+  CHECK("134567" == r.chars);
 }
